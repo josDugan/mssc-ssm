9ee0311e4a94ddb61160c5857bca4feafbf8799c
josDugan <josephdug@gmail.com>
2020-08-30 15:12:47 -0400

    Add Payment service interface

---
+++ src/main/java/guru/springframework/services/PaymentService.java
@@ -0,0 +1,17 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Payment;
+import guru.springframework.domain.PaymentEvent;
+import guru.springframework.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+public interface PaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+}
