e5f50ee29e86aeafca8b168c9d00315933467475
josDugan <josephdug@gmail.com>
2020-08-31 14:35:40 -0400

    Add payment service and interceptor.

---
+++ src/main/java/guru/springframework/services/PaymentServiceImpl.java
@@ -0,0 +1,83 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Payment;
+import guru.springframework.domain.PaymentEvent;
+import guru.springframework.domain.PaymentState;
+import guru.springframework.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    static final String PAYMENT_ID_HEADER = "payment_id";
+
+    private final PaymentRepository repository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    private final PaymentStateChangeInterceptor paymentStateChangeInterceptor;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(PaymentState.NEW);
+        return repository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
+
+        return sm;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_APPROVED);
+
+        return sm;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_DECLINED);
+
+        return sm;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = repository.getOne(paymentId);
+
+        StateMachine<PaymentState,PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(accessor -> {
+                    accessor.addStateMachineInterceptor(paymentStateChangeInterceptor);
+                    accessor.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+
+        sm.start();
+
+        return sm;
+    }
+
+    private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event) {
+        Message msg = MessageBuilder.withPayload(event)
+                .setHeader(PAYMENT_ID_HEADER, paymentId)
+                .build();
+
+        sm.sendEvent(msg);
+    }
+}
---
+++ src/main/java/guru/springframework/services/PaymentStateChangeInterceptor.java
@@ -0,0 +1,36 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Payment;
+import guru.springframework.domain.PaymentEvent;
+import guru.springframework.domain.PaymentState;
+import guru.springframework.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentStateChangeInterceptor extends StateMachineInterceptorAdapter<PaymentState, PaymentEvent> {
+
+    private final PaymentRepository paymentRepository;
+
+    @Override
+    public void preStateChange(State<PaymentState, PaymentEvent> state, Message<PaymentEvent> message, Transition<PaymentState, PaymentEvent> transition, StateMachine<PaymentState, PaymentEvent> stateMachine) {
+        Optional.ofNullable(message).ifPresent(
+                msg -> {
+                    Optional.ofNullable(Long.class.cast(msg.getHeaders().getOrDefault(PaymentServiceImpl.PAYMENT_ID_HEADER, -1L)))
+                            .ifPresent(paymentId ->{
+                                Payment payment = paymentRepository.getOne(paymentId);
+                                payment.setState(state.getId());
+                                paymentRepository.save(payment);
+                            });
+                }
+        );
+    }
+}
